3450dc5fe67213ebb916bdd7bfd837cd4cfbd1ae
Pierrick Pamart <pierrick.pamart@telecom-sudparis.eu>
2018-08-18 10:08:05 +0200

    Cleaned the code

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ At the end the bot should be able to:
 - Maintain a list of game for the server
 - Create and manage a certain amount of channel for each game
 - Manage the roles/permission to hide/display the channels to users
+
+Do not forget to put your token in the data.json as:
+{"token": "YOUR_TOKEN_HERE"}
+
+It is just in development but I'm working on it
--- botGamint.py
+++ botGamint.py
@@ -11,32 +11,32 @@ with open('data.json') as json_data:
 
 client = Bot(command_prefix=BOT_PREFIX)
 
+
 class Game:
 
-
-    def __init__(self, name, mainNickName = None):
+    def __init__(self, name, main_nick_name=None):
         self.name = name
         self.nicknames = []  # The 1st one il the "main nickname" which will serve to createsub channel
-        print(mainNickName)
-        if not mainNickName:
-            mainNickName = name
-        self.nicknames.append(mainNickName)
+        print(main_nick_name)
+        if not main_nick_name:
+            main_nick_name = name
+        self.nicknames.append(main_nick_name)
 
-    def addNickname(self, nick):
+    def add_nickname(self, nick):
         self.nicknames.append(str(nick))
 
-    def getNicknames(self):
+    def get_nicknames(self):
         return self.nicknames
 
-    def getTupleForHelp(self):
-        return (self.name, self.nicknames[0])
+    def get_tuple_for_help(self):
+        return self.name, self.nicknames[0]
 
-    def isTheOne(self, nameSearched):
-        #print(self.name, nameSearched, self.nicknames)
-        return self.name == nameSearched or nameSearched in self.nicknames
+    def is_the_one(self, name_searched):
+        return self.name == name_searched or name_searched in self.nicknames
 
-    def getChanRoot(self):
+    def get_chan_root(self):
         return str(self.name).replace(" ", "-")
+
 
 class GameList:
     gameList = []
@@ -46,26 +46,27 @@ class GameList:
         pass
 
     def add(self, name, nickname=None):
-        '''
+        """
         :param name: nom du jeu
         :param nickname: list des surnoms du jeu
         :return:
-        '''
-        if not (self.findGame(name) or self.findGame(nickname)):
+        """
+
+        if not (self.find_game(name) or self.find_game(nickname)):
             self.gameList.append(Game(name, nickname))
             return True
         else:
             print("Name or Nickname already exist")
             return False
-        #TODO: créer les chan
+        # TODO: créer les chan
 
-    def getList(self):
+    def get_list(self):
         for e in self.gameList:
-            yield e.getTupleForHelp()
+            yield e.get_tuple_for_help()
 
-    def findGame(self,search):
+    def find_game(self, search):
         for e in self.gameList:
-            if e.isTheOne(search):
+            if e.is_the_one(search):
                 print(e.name + " found!")
                 return e
         return None
@@ -74,8 +75,7 @@ class GameList:
 gameList = GameList()
 gameList.add("League of Legends", "lol")
 gameList.add("Counter Strike Global Offensive", "csgo")
-    #{"League of Legends": "LoL", "Dota 2": "Dota"}
-
+# {"League of Legends": "LoL", "Dota 2": "Dota"}
 
 
 @client.command(name="jeux",
@@ -84,31 +84,33 @@ gameList.add("Counter Strike Global Offensive", "csgo")
                 aliases=["games, roles"],
                 pass_context=False)
 # Retourne la liste de tout les jeux disponibles sur le serveur
-async def getGameList():
-    str = "Les jeux diponnibles sont:\n"
-    for e in gameList.getList():
-        str += "- " + e[0] + " (" + e[1] + ")\n"
+async def get_game_list():
+    string = "Les jeux diponnibles sont:\n"
+    for e in gameList.get_list():
+        string += "- " + e[0] + " (" + e[1] + ")\n"
     # str ="\n- ".join(list(gameList.keys()) + " (" + gameList.values() + ")")
     # str = "Les jeux diponnibles sont:\n- " + str
-    await client.say(str)
+    await client.say(string)
 
-#TODO: Ajouter la vérifications des permissions
+
+# TODO: Ajouter la vérifications des permissions
 @client.command(name="nouveau_jeu",
                 description="Ajoute un jeu à la liste, et créer leschans correspondants",
                 brief="Réservé aux admin",
                 aliases=["nJeu"],
                 pass_context=False)
-async def addGameToList(game, nickname=None):
+async def add_game_to_list(game, nickname=None):
     gameList.add(game, nickname)
     await client.say(game + " successfuly added !")
+
 
 @client.command(name="isPresent",
                 description="Dit si le jeu est présent ou non",
                 brief="Dit si le jeu est présent ou non",
                 aliases=["isP"],
                 pass_context=False)
-async def searchGame(search):
-    result = gameList.findGame(search)
+async def search_game(search):
+    result = gameList.find_game(search)
     if result:
         await client.say(result.name + " found !")
         return
